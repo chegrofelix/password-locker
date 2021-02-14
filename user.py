bf0de54a1bca2a9631b8340e7cf73dce60178d97
chegrofelix <chegerofelix@gmail.com>
2021-02-14 13:37:56 +0300

    created class user

---
+++ user.py
@@ -0,0 +1,6 @@
+class user:
+
+    def __init__ (self, username, password):
+        self.username = str(input("Enter your preferable username :"))
+        self.password = input("Create password")
+ 
