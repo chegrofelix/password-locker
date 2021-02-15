fa575b7930e78e8fbcc9157e2a5c52b569561014
chegrofelix <chegerofelix@gmail.com>
2021-02-15 11:32:34 +0300

    added code to class user and class credentials

--- credentials.py
+++ credentials.py
@@ -1,11 +1,65 @@
-class credentials:
-     
-     def __init__(self, application, username, password):
-         self.application = application
-         self.username = username
-         self.password = password
+import random
+import string
+import pyperclip
+class Credential:
+        """
+    Class that generates new instances of credentials.
+        """
 
-application = input("Enter the application name :")
-username = input("Enter your username for the application :")
-password = input("Enter your password for the application :")
+        Credential_list = [] # empty credentials list
+
+
+    def __init__(self,account,username, password):
+        """
+        method that defines user credential to be stored
+        """
+        self.account = account
+        self.username = username
+        self.password = password
+
+    def save_credential(self):
+        '''
+        method that saves credential objects into application
+        '''
+        Credential.credential_list.append(self)
+
+    def delete_credential(self):
+        '''
+        method deletes a saved credential from credential_list
+        '''
+
+        Credential.Credential_list.remove(self)
+
+
+    @classmethod
+    def find_credentials(cls,account):
+        """
+        method that takes in account_name and returns a credentials that matches account_name
+        """
+        for Credential in cls.credential_list:
+            if Credential.account == account:
+                return Credential
+
+    @classmethod
+    def credential_exist(cls,name):
+         '''
+         method that checks if a credential exists from the credetial list
+         Args:
+            name: name to search if it exists
+         Returns:
+            Boolean: True or false depending if credential exists
+         ''' 
+
+         for Credential in cls.credential_list:
+             if credential_name == name:
+                 return True
+
+        return False          
+
+    @classmethod
+    def display_credential(cls):
+        '''
+        method that returns the credential list
+        '''
+        return cls.credential_list
  
--- user.py
+++ user.py
@@ -1,10 +1,56 @@
-class user:
 
-    def __init__ (self, username, password):
-        self.username = username 
+
+ class User:
+    """
+    Class that generates new instances of user
+    """
+
+    User_list [] # Empty user list
+
+    def __init__(self,username, password): #method that helps us define properties for our objects.
+    
+        self.username = username
         self.password = password
-        
-username = str(input("Enter your preferable username :"))
-password = input("Create password :")
 
- 
+    def save_user(self):
+        '''
+        save new user to the application
+        '''
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+    @classmethod
+    def display_all_users(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list
+
+    @classmethod
+    def find_by_username(cls,username):
+        '''
+        Method that takes in a username and returns a user that matches that username.
+        Args:
+            username: user to search for
+        Returns :
+            user  that matches the username.
+        '''
+
+        for user in cls.user_list:
+            if user.username == username:
+                return user
+
+    @classmethod
+    def user_exist(cls,username):
+        '''
+        checks if a given user exists
+        '''
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
