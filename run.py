ff6bbc5da615440ec2c9d5603c18e8b9c532a775
chegrofelix <chegerofelix@gmail.com>
2021-02-15 16:14:48 +0300

    added code to run.py

--- run.py
+++ run.py
@@ -1,173 +1,156 @@
-import getpass
-import string
+#!/usr/bin/env python3.8
 
-from user import User
+import os
 from credential import Credential
+from user import User
+import random
 
-def create_user(username,password):
-    '''
-    function to create  new user
-    '''
-    new_user= User(username,password)
-    return new_user
-    
-def save_user(user):
-    '''
-    function to save new user
-    '''
-    User.save_user()
-        
-def delete_user(user):
-    '''
-    function that deletes a user
-    '''
-    User.delete_user() 
-        
-def find_user(username):
-    '''
-    function that finds a user
-    '''
-    return User.find_by_username( username)
-    
-def check_existing_users(username):
-    '''
-    function that checks if a user exist
-    '''
-    return User.user_exist(username)
-    
-def display_all_users():
-    '''
-    function that returns all the saved users in the application
-    '''
-    return User.user.display_all_users()
+# def create_credentials(first_name, last_name, user_email):
+#     '''
+#     A function that creates a new users credentials
+#     '''
 
+#     new_user_credential = Credential(first_name, last_name, user_email,)
+#     return new_user_credential
 
-def create_credential(self, account, username, password):
-    '''
-    Function to create a new credential
-    '''
-    new_credential = Credential( account, username, password)
-    return new_credential
+# def create_user_account(first_name, last_name, user_name, user_email, user_password):
+#     '''
+#     Function that creates a new user account
+#     '''
+#     new_user_account = User(first_name, last_name, user_name, user_email, user_password)
+#     return new_user_account
 
+# def save_credential(credential):
+#     '''
+#     A function that saves the new users credentials
+#     '''
+#     credential.save_credential()
 
-def save_credential(credential):
-    '''
-    Function to save credential
-    '''
-    credential.save_credential()
+# def save_user_account(user):
+#     '''
+#     A function that saves the new users account details
+#     '''
+#     user.save_user_account()
 
+# def delete_credential(credential):
+#     '''
+#     A function that deletes the new user's credentials
+#     '''
+#     credential.delete_credential()
 
-def delete_credential(credential):
-    '''
-    Function to delete a credential
-    '''
-    Credential.credential_list.remove(self)
+# def search_user_email(credential):
+#     '''
+#     A function that searches for a user's credentials
+#     '''
+#     credential.search_user_email()
 
+# def credential_exist(credential):
+#     '''
+#     A function that checks if the credentials of the email being searched for exists
+#     '''
+#     credential.credential_exist()
 
-def find_credential(username):
-    '''
-    Function that finds a credential by username and returns the credential
-    '''
-    return Credential.find_credential(username)
+# def display_credential(credential):
+#     '''
+#      display_credential this method that retuns the list of credential_list
+#      '''
+#     credential.display_credential()
 
-
-def check_existing_credential(username):
-    '''
-    Function that check if a credential exists with that username and return a Boolean
-    '''
-    return Credential.credential_exist(username)
-
-
-def display_credential():
-    '''
-    Function that returns all the saved credential
-    '''
-    return Credential.display_credential()
-
+# def display_user_account(user):
+#     '''
+#     A function that displays the user's account 
+#     '''
+#     user.display_user_account()
 
 def main():
-    print("Hi! welcome to an application that help you manage your credentials")
-    print("\n")
-    print('please use the following short codes to perform any task you want ca = create account,' )
-    short_code = input().lower()
-    if short_code == 'ca':
-        print('Create  your account details here')
-        print('*' * 10)
-        username = input('Enter  your Username: ')
-        while True:
-            print('use :  ep = to  enter your own password')
-            password_choice = input().lower()
-            if password_choice == 'ep':
-                password = input('Enter Password: ')
-                break
-            else:
-                print('You have entered a wrong short code.  try again')
+    print("Hi,Hello!! Welcome PassLock Credentials Check. Enter User First Name.")
+    first_name = input()
 
-                save_user(create_user(username, password))
-        print('*' * 10)
-        print(f'Welcome {username} to Password locker.Your new account  password is <--- {password} --->')
-        print('*' * 10)
-    while True:
-        print('Use the following  short codes to manage  your credentials: \n cc = create credential, \n dc = display credentials,\n fc =     find credential  \n Dd = delete credential, \n  EX = exit application')
-        short_code = input().lower()
-        if short_code == 'cc':
-            print('Enter your new credential details')
-            print('*' * 10)
-            account = input('Account Name : ')
-            username1 = input('Username : ')
-            while True:
-                print('Use: ep =  enter  your own password?')
-                password_choice = input().lower()
-                if password_choice == 'ep':
-                    password = input('Enter password : ')
+    print(f"Hey {first_name} you're a step into PassLock. Please enter your last name.")
+    last_name = input()
+
+    print(f"Hey {first_name} {last_name} input your credential user email ")
+    user_email = input()
+
+    user_in = int(input("Reply with 1 to enter password or 2 to generate a new password for you"))
+    if user_in ==1:
+        password = input("Enter password: ")
+        conf_pass = input("Confirm password: ")
+        while password !=conf_pass:
+            print("password does not match")
+            conf_pass = input("Confirm password: ")
+    else:
+        pass_len = int(input("Specifiy password length: "))
+        password = User.generate_password(pass_len)
+        print(f"The generated password is {password}")
+
+    print(f"Congratulations {first_name} {last_name}! Welcome to PassLock!!")
+    print('\n')
+    account_user = User(first_name,last_name,user_email,password)
+
+    while 1:
+        print('*'*70)
+        print('*'*70)
+        print("For easier navigation through PassLock use the short codes below:\n cc - create an account with a user-define password \n dis - display credentials \n del - delete account \n va - view account \n ex - Exit PassLock ")
+        short_code = input()
+        short_code = short_code.lower()
+        print("short_code",short_code)
+
+        if short_code == "cc":
+            
+            print("What account would you like to add or create credentials for??ie Twitter, Instagram or Palmchat")
+            account_name = input()
+
+            print("Username")
+            username = input()
+
+            print("1. Enter password\n2. Generated Password")
+            user_in = int(input())
+            if user_in ==1:
+                password = input("Enter password: ")
+            else:
+                pass_len = int(input("Password length: "))
+                password = account_user.generate_password(pass_len)
+                print(f"generated password is '{password}'")
+            account_user.save_credential(account_name,username,password)           
+            print(f"A {account_name} has successfully been created.")
+            print(f"The user account name is {account_name} and the password {password}")
+            print('\n')
+
+        elif short_code == "dis":
+                if account_user.display_credential() !=[]:
+                    print("Here are your User Account Credentials")
+                    for account in account_user.display_credential():
+                        print(account,'\n')
+                
+                else:
+                    print("Oops!! Credentials not found Please Create an Account using the short code cp")
+                    print('\n')
+
+        elif short_code=="del":
+            while 1:
+                user_in = input("Account name: ")
+                if account_user.credential_exist(user_in):
+                    account_user.delete_credential(user_in)
+                    print("account deleted successfully!")
                     break
                 else:
-                    print('You have entered a wrong  short code. kindly try again')
-                print('*' * 10)
-            save_credential(create_credential(account, username1, password,password))
-            print('*' * 10)
-            print(f'Your {account} account details has been saved in our application')
-            print('*' * 10)
-        elif short_code == 'dc':
-            if display_credential():
-                print('Your  credentials are as follows:')
-                for account in display_credential():
-                    print('-' * 10)
-                    print(f'Username: {username1} \n Password: {password}')
-                    print('-' * 10)
-            else:
-                print('*' * 10)
-                print('You have no account. Please Create a new account')
-                print('*' * 10)
-        elif short_code == 'dd':
-            print('Enter Account name to delete...')
-            name = input('Acount Name : ')
-            print('*' * 10)
-            if find_credential(name):
-                name_result = find_credential(name)
-                name_result.delete_credential()
-                print(f'Account {name} has been deleted successfully ')
-                print('*' * 10)
-            else:
-                print('Incorrect account name.kindly type the correct again')
-                print('*' * 10)
-        elif short_code == 'fc':
-            print('Enter Account Name To Search...')
-            search = input('Account Name : ')
-            print('*' * 10)
-            if find_credential(search):
-                search = find_credential(search)
-                print(f'Account Name: {search} ')
-                print('*' * 10)
-            else:
-                print(' the Credential  you are searching does not exist')
-                print('*' * 10)
-        elif short_code == 'ex':
-            print('thanks for using our password-locker to keep your credentials')
-            print('*' * 10)
+                    print("account does not exist")
+        elif short_code == "ex":
+            account_user.logout()
+            print("PASSLOCK BIDS BYE....THANK YOU FOR VISITING")
             break
+        elif short_code == 'va':
+            while 1:
+                user_in = input("Account name: ")
+                if account_user.credential_exist(user_in):
+                    print(account_user.view_account(user_in))
+                    break
+                else:
+                    print("Account does not exist")
+
         else:
-            print(' You have entered invalid Short code. Kindly  try again!')
-            print('*' * 10)
+            print("Invalid short code please use: \n cc - create an account with a user-define password \n dis - display credentials \n cp - create a PassLock account with an auto-generated password \n va - view account \n ex - Exit PassLock ")
+
 if __name__ == '__main__':
     main()
